14ad767356e158da72da340e863539c3d10bc34a
Martin Ohmann <martin.ohmann@lesara.de>
2018-07-19 08:38:22 +0200

    [master] fix example script

--- doc/example.php
+++ doc/example.php
@@ -1,22 +1,29 @@
 <?php
 
-use ArrayTransform\Loader\YamlLoader;
 use ArrayTransform\Mapping\MappingFactory;
 use ArrayTransform\Transformer\Transformer;
 
-require_once dirname(dirname(__FILE__)) . '/vendor/autoload';
+require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
 
 try {
-    $loader = new YamlLoader();
-    $config = $loader->load(dirname(__FILE__) . '/example-mapping.yaml');
+    $mappingFile = dirname(__FILE__) . '/example-mapping.yaml';
 
     $factory = new MappingFactory();
-
-    $mapping = $factory->createMapping($config);
+    $mapping = $factory->createMappingFromFile($mappingFile);
 
     $transformer = new Transformer($mapping);
 
-    $result = $transformer->transform(['foo' => 1]);
+    $result = $transformer->reverseTransform([
+        'somekey' => 100,
+    ]);
+
+    var_dump($result);
+
+    $result = $transformer->transform([
+        'some' => [
+            'key' => 20,
+        ],
+    ]);
 
     var_dump($result);
 } catch (\Exception $e) {
