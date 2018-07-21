e70b4826f12e29c15e914896f7e44f43c056efee
Martin Ohmann <martin.ohmann@lesara.de>
2018-07-21 16:40:36 +0200

    [master] remove duplicate license header

--- src/Mapping/Mapping.php
+++ src/Mapping/Mapping.php
@@ -8,15 +8,6 @@
  * file that was distributed with this source code.
  */
 
-/*
- * This file is part of the array-transform package.
- *
- * (c) Martin Ohmann <martin@mohmann.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace ArrayTransform\Mapping;
 
 use ArrayTransform\Rule\RuleInterface;
