fdf1a6d856327fe3d7648e84b7d5b31cf0f5aff8
Martin Ohmann <martin.ohmann@lesara.de>
2018-07-21 02:49:50 +0200

    [master] add license header

--- src/Loader/YamlLoader.php
+++ src/Loader/YamlLoader.php
@@ -1,4 +1,12 @@
 <?php declare(strict_types=1);
+/*
+ * This file is part of the array-transform package.
+ *
+ * (c) Martin Ohmann <martin@mohmann.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace ArrayTransform\Loader;
 
